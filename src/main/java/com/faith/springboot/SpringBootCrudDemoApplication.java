81cf93e2d993dd5d44e1edefe7ecb12ed3e71cc2
sahadan <sahadan@gmail.com>
2022-10-29 11:06:28 +0530

    Initial Commit

---
+++ src/main/java/com/faith/springboot/SpringBootCrudDemoApplication.java
@@ -0,0 +1,14 @@
+package com.faith.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudDemoApplication.class, args);
+		System.out.println("Welcome to Hibernate-REST-MySQL-Maven-Demo");
+	}
+
+}
